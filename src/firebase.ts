1836678e24a16a2fae81dbe6ea60bd8de96b684a
leneves-crypto <leneves@alupar.com.br>
2026-04-17 03:33:54 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBP2NTJHjjxKtHwsAeJfFU27v1dzyp40Hw",
-  authDomain: "gen-lang-client-0813022331.firebaseapp.com",
-  projectId: "gen-lang-client-0813022331",
-  storageBucket: "gen-lang-client-0813022331.firebasestorage.app",
-  messagingSenderId: "1074424898743",
-  appId: "1:1074424898743:web:c13179d463424a4264d932"
+  apiKey: "COLE_AQUI_A_SUA_API_KEY",
+  authDomain: "ai-studio-b9f262fe-d0eb-443c-8760-386f6d10f2cd.firebaseapp.com",
+  projectId: "ai-studio-b9f262fe-d0eb-443c-8760-386f6d10f2cd",
+  storageBucket: "ai-studio-b9f262fe-d0eb-443c-8760-386f6d10f2cd.appspot.com",
+  messagingSenderId: "COLE_AQUI_O_SENDER_ID",
+  appId: "COLE_AQUI_O_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
