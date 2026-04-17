1b1e773a6a020c1e6d2ae2b48913a7d36a68e1c5
leneves-crypto <leneves@alupar.com.br>
2026-04-17 03:32:29 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import firebaseConfig from '../firebase-applet-config.json';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBP2NTJHjjxKtHwsAeJfFU27v1dzyp40Hw",
+  authDomain: "gen-lang-client-0813022331.firebaseapp.com",
+  projectId: "gen-lang-client-0813022331",
+  storageBucket: "gen-lang-client-0813022331.firebasestorage.app",
+  messagingSenderId: "1074424898743",
+  appId: "1:1074424898743:web:c13179d463424a4264d932"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
